cddb6b89410f38203bfd6549971ec12e84f73d6c
Natalya <ngkolyadina@1367.ru>
2021-08-11 14:58:44 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse ("Это главная страница соц-сети для блорегов :)")
+
+
+def group_posts(request, slug):
+    return HttpResponse("А это страница с постами пользователей, которые отсортированы по группам.")
